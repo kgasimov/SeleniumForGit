a70cb7dcf983ab85aba1abd5626ff4992a95b02d
Kamran Gasimov <kgasimov@gmail.com>
2021-05-22 11:20:20 -0500

    Some changes made to reverse code

--- src/test/java/GitPractice/MySecondClass.java
+++ src/test/java/GitPractice/MySecondClass.java
@@ -10,8 +10,6 @@ public class MySecondClass {
         for (int i = 0; i < arr.length; i++) {
             for (int j = arr[i].length()-1; j >=0; j--) {
 
-                arr2[j] = arr[i];
-
             }
         }
         System.out.println(Arrays.toString(arr2));
