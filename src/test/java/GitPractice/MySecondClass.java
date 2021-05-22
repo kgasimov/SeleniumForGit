975af9c106c16f5d9453d9063add37a867800795
Kamran Gasimov <kgasimov@gmail.com>
2021-05-22 11:19:35 -0500

    Array reverse code added

---
+++ src/test/java/GitPractice/MySecondClass.java
@@ -0,0 +1,19 @@
+package GitPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MySecondClass {
+    public static void main(String[] args) {
+        String [] arr = {"java", "selenium", "git", "jira"};
+        String [] arr2 = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i].length()-1; j >=0; j--) {
+
+                arr2[j] = arr[i];
+
+            }
+        }
+        System.out.println(Arrays.toString(arr2));
+    }
+}
